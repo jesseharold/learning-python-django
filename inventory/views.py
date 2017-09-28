dae9ff7d2487b1d8a1ec6b765c261ac9afb5be13
jesseharold <jesseharold@gmail.com>
2017-09-27 23:47:44 -0700

    completed views, add basic templates

--- inventory/views.py
+++ inventory/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+
+from inventory.models import Item
 
 # these views are definted in urls.py
 
 def index(request):
-    return HttpResponse('<p>In index view</p>')
+    items = Item.objects.exclude(amount=0)
+    return render(request, 'inventory/index.html', {
+        'items': items,
+    })
 
 def item_detail(request, id):
-    return HttpResponse('<p>In item_detail view with id {0}</p>'.format(id))
+    try:
+        item = Item.objects.get(id=id)
+    except Item.DoesNotExist:
+        raise Http404("This item does not exist")
+    return render(request, 'inventory/item_detail.html', {
+        'item': item,
+    })
 
 # the curly braces followed by .format is called string interpolation, and it puts the value of id into the braces slot. you can have more than one value, which is why it's a number, 0 is the first value
